b5494b0aecca42cd25dcc0645d208e8424e3e13e
Marshal Stewart <pontiacmarshal@gmail.com>
2020-07-26 21:31:37 -0400

    Initialized repo.

---
+++ cmake-build-debug/CMakeFiles/chip8emu.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chip8emu.dir/main.cpp.obj"
+  "chip8emu.exe"
+  "chip8emu.exe.manifest"
+  "chip8emu.pdb"
+  "libchip8emu.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chip8emu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
